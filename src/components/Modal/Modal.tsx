bae699127bba1a66bb9fca016e06ec59d5a8e35b
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2024-01-30 01:13:39 +0200

    Fix scroll bug on modal and esc key functionality

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import styles from "./Modal.module.css";
 
 interface ModalProps {
@@ -8,24 +8,46 @@ interface ModalProps {
   filterOptionName: string;
 }
 
-const Modal = ({ children, onClose, isOpen, filterOptionName }: ModalProps) => (
-  <div
-    className={`${styles.modalOverlay} ${
-      isOpen ? `${styles.active} ${styles["hide-scroll"]}` : ""
-    }`}
-    onClick={onClose}
-  >
+const Modal = ({ children, onClose, isOpen, filterOptionName }: ModalProps) => {
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.body.classList.add("no-scroll");
+      window.addEventListener("keydown", handleKeyDown);
+    } else {
+      document.body.classList.remove("no-scroll");
+      window.removeEventListener("keydown", handleKeyDown);
+    }
+    return () => {
+      document.body.classList.remove("no-scroll");
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [isOpen, onClose]);
+
+  return (
     <div
-      className={`${styles.modalContent} ${isOpen ? styles.active : ""}`}
-      onClick={(e) => e.stopPropagation()}
+      className={`${styles.modalOverlay} ${
+        isOpen ? `${styles.active} ${styles["hide-scroll"]}` : ""
+      }`}
+      onClick={onClose}
     >
-      <h2 className={styles.heading}>{filterOptionName}</h2>
-      <div className={styles.container}>{children}</div>
-      <button className={styles.readyButton} onClick={onClose}>
-        &#10004; Потвърди
-      </button>
+      <div
+        className={`${styles.modalContent} ${isOpen ? styles.active : ""}`}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <h2 className={styles.heading}>{filterOptionName}</h2>
+        <div className={styles.container}>{children}</div>
+        <button className={styles.readyButton} onClick={onClose}>
+          &#10004; Потвърди
+        </button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Modal;
